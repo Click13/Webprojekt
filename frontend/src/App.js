915087331019b55cca3ad1a7bf4a71a697f05230
Wolf, Janik <janik.markus.wolf@accenture.com>
2021-09-23 09:24:39 +0200

    Add React Project and Signin and Signup Page

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Main from './components/Main';
+
+function App() {
+  return (<Router>
+    
+    
+          <Switch>
+            <Route exact path='/' component={Main} />
+            <Route path="/sign-in" component={SignIn} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+    
+    </Router>
+  )}
+  export default App;
